8a223b305c67f721e85fcbce72c17263cd326096
Zion0707 <Zion19920707@gmail.com>
2017-05-16 12:06:31 +0800

    增加金额转换

--- js/jq.toolCore.js
+++ js/jq.toolCore.js
@@ -7,7 +7,6 @@ ToolCore.CONFIG = {
 
 }
 
-
 /**
 * cookie 操作函数
 * @param set : 设置cookie
@@ -457,7 +456,6 @@ ToolCore.futureDay = function( day ){
 
 
 
-
 /*
 * 日期操作
 *@namespace oofUtil.date
@@ -689,3 +687,21 @@ ToolCore.objDeepCopy = function (source) {
     return sourceCopy;
 }
 
+
+/*
+* 金额阿拉伯数字转中文
+* @param n 数字
+**/
+ToolCore.NumberDx = function(n){
+    if (!/^(0|[1-9]\d*)(\.\d+)?$/.test(n))
+        return "数据非法";
+    var unit = "千百拾亿千百拾万千百拾元角分", str = "";
+        n += "00";
+    var p = n.indexOf('.');
+    if (p >= 0)
+        n = n.substring(0, p) + n.substr(p+1, 2);
+        unit = unit.substr(unit.length - n.length);
+    for (var i=0; i < n.length; i++)
+        str += '零壹贰叁肆伍陆柒捌玖'.charAt(n.charAt(i)) + unit.charAt(i);
+    return str.replace(/零(千|百|拾|角)/g, "零").replace(/(零)+/g, "零").replace(/零(万|亿|元)/g, "$1").replace(/(亿)万|壹(拾)/g, "$1$2").replace(/^元零?|零分/g, "").replace(/元$/g, "元整");
+}
